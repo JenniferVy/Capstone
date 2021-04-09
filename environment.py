e9ae25f05598143f893607a7d70abb046506b3f0
JenniferVy <36561447+JenniferVy@users.noreply.github.com>
2021-04-08 23:58:54 -0700

    Skeleton Environment

---
+++ environment.py
@@ -0,0 +1,18 @@
+############################### Environment ###################################
+
+ENVIRO_LENGTH = 500
+ENVIRO_WIDTH = 500
+
+class Environment:
+  def __init__(self, length = ENVIRO_LENGTH, width = ENVIRO_WIDTH):
+    self.areaL = length
+    self.areaW = width
+    # TODO: define trash size type and corresponding ranges
+
+  def generateTrashGrid(self):
+    # TODO: generate grid of trash
+    return []
+
+  def plotTrashGrid(self):
+    # TODO: visualize grid of trash
+    return []
